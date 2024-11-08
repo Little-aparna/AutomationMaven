dc569f7e27e004324a8a0a74d8d6c22418bd71da
Little-aparna <aparnaks991@gmail.com>
2024-11-08 20:06:58 +0530

    commit

--- AutomationMaven/src/test/java/testscripts/WebElementcommands.java
+++ AutomationMaven/src/test/java/testscripts/WebElementcommands.java
@@ -3,12 +3,50 @@ package testscripts;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 
+
 public class WebElementcommands extends Base {
-public void webElementCommand()
+public void webElementCommand() 
 {
 	
+
 	WebElement showMessageButton=driver.findElement(By.id("button-one"));
-	}
+	WebElement singleInputfield=driver.findElement(By.id("single-input-field")) ;
+	WebElement yourMessageButton=driver.findElement(By.id("message-one"));
+	WebElement twoInputField=driver.findElement(By.id("message-two"));
+	WebElement ajaxFormSubmit=driver.findElement(By.id("button-two"));
+	
+	
+
+	WebElement className=driver.findElement(By.className("clearfix"));
+	WebElement className1=driver.findElement(By.className("top-logo"));
+	WebElement className2=driver.findElement(By.className("copyright"));
+	WebElement className3=driver.findElement(By.className("mb-sec"));
+	
+	
+WebElement nameVariable=driver.findElement(By.name("viewport"));
+WebElement nameVariable1=driver.findElement(By.name("author"));
+WebElement nameVariable2=driver.findElement(By.name("description"));
+WebElement nameVariable3=driver.findElement(By.name("keywords"));
+
+WebElement tagNameSample=driver.findElement(By.tagName("header"));
+WebElement tagNameSample1=driver.findElement(By.tagName("section"));
+
+WebElement linkTextSample=driver.findElement(By.linkText("Checkbox Demo"));
+
+WebElement enterMessage=driver.findElement(By.linkText("Simple Form Demo"));
+WebElement linkTextSample1=driver.findElement(By.linkText("Select Input"));
+WebElement linkTextSample2=driver.findElement(By.linkText("Ajax Form Submit"));
+
+
+WebElement partialTextSample=driver.findElement(By.partialLinkText("Checkbox"));
+WebElement partialTextSample1=driver.findElement(By.partialLinkText("Radio Buttons"));
+WebElement partialTextSample2=driver.findElement(By.partialLinkText("Simple Form "));
+
+
+
+
+}
+
 	
 	public static void main(String[] args) {
 	
