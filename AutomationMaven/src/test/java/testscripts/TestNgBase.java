49db42ada4d9183243e6d98f4aa03237bb06162b
Little-aparna <aparnaks991@gmail.com>
2024-11-28 19:32:42 +0530

    commit

--- AutomationMaven/src/test/java/testscripts/TestNgBase.java
+++ AutomationMaven/src/test/java/testscripts/TestNgBase.java
@@ -4,9 +4,12 @@ import java.time.Duration;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
 
 public class TestNgBase {
          public WebDriver driver;
+         @BeforeMethod
 	public void initializeBrowser()
 	{
 		 driver=new ChromeDriver();
@@ -15,20 +18,16 @@ public class TestNgBase {
 		driver.manage().window().maximize();
 		
 		}
+	@AfterMethod
 	public void driverQuit()
 	{
 		driver.quit();
-		//driver.close();
+		
 		}
 	
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-		TestNgBase testngbase=new TestNgBase();
-		testngbase.initializeBrowser();
-		testngbase.driverQuit();
+	
 		
 		
 	}
 
-}
+
